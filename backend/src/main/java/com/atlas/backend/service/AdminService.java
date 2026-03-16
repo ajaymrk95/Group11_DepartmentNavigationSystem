85cf9973730b6c3bb07aec10d34032569a9aee24
tarunpradeep <tarunpradeep2005@gmail.com>
2026-03-17 03:15:36 +0530

    feat:added admin login backend and admin table to db

---
+++ backend/src/main/java/com/atlas/backend/service/AdminService.java
@@ -0,0 +1,29 @@
+package com.atlas.backend.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.atlas.backend.model.Admin;
+import com.atlas.backend.repository.AdminRepository;
+
+@Service
+public class AdminService {
+
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository){
+        this.adminRepository = adminRepository;
+    }
+
+    public boolean login(String username, String password){
+
+        Optional<Admin> admin = adminRepository.findByUsername(username);
+
+        if(admin.isPresent()){
+            return admin.get().getPassword().equals(password);
+        }
+
+        return false;
+    }
+}
