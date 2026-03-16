c89a5958c57b610b88c5cf537c8802e8d393133e
tarunpradeep <tarunpradeep2005@gmail.com>
2026-03-17 01:29:38 +0530

    feat: add atlas dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import Dashboard from "./pages/Dashboard";
+import AdminLogin from "./pages/AdminLogin";
 import OutdoorNav from "./pages/OutdoorNav"
 import AddLocation from "./pages/AddLocation"
 import EditLocation from "./pages/EditLocation"
@@ -10,8 +11,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-      
-        <Route path="/" element={<OutdoorNav/>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/outdoor-navigation" element={<OutdoorNav/>} />
         <Route path="/add-location" element={<AddLocation />} />
         <Route path="/edit-location" element={<EditLocation />} />
       
